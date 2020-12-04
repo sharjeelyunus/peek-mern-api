48e20b11842d79ae04d79d4cd7ce2724fd635f30
sharjeelyunus <sharjeel924@gmail.com>
2020-12-04 15:16:25 +0500

    👌 IMPROVE: Adding Place-Specific Routes

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
-app.use(placesRoutes);
+app.use('/api/places', placesRoutes);
 
 app.listen(5000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    console.log('GET Request in Places');
-    res.json({message: 'It works!'});
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        location: {
+            lat: 40.7484474,
+            lng: -73.9871516
+        },
+        address: '20 W 34th St, New York, NY 10001',
+        creater: 'u1'
+    }
+];
+
+router.get('/:pid', (req, res, next) => {
+    const placeId = req.params.pid;
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId;
+    });
+    res.json({place});
 });
 
 module.exports = router;
