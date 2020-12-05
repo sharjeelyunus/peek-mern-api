d89c5499af169f88de5b76f51b6103dd14062a25
sharjeelyunus <sharjeel924@gmail.com>
2020-12-05 18:19:47 +0500

    👌 IMPROVE: Getting a Place By User ID

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -21,7 +21,17 @@ router.get('/:pid', (req, res, next) => {
     const place = DUMMY_PLACES.find(p => {
         return p.id === placeId;
     });
-    res.json({place});
+    res.json({ place });
+});
+
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid;
+
+    const place = DUMMY_PLACES.find(p => {
+        return p.creater === userId;
+    });
+
+    res.json({ place });
 });
 
 module.exports = router;
