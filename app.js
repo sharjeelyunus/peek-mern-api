644510196a6e7238d3e8aa4060b471e15e3c4c70
sharjeelyunus <sharjeel924@gmail.com>
2020-12-06 15:31:19 +0500

    📦 NEW: Handling Errors

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ const app = express();
 
 app.use('/api/places', placesRoutes);
 
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({message: error.message || 'An unknown error occured!'});
+});
+
 app.listen(5000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -18,9 +18,17 @@ const DUMMY_PLACES = [
 
 router.get('/:pid', (req, res, next) => {
     const placeId = req.params.pid;
+
     const place = DUMMY_PLACES.find(p => {
         return p.id === placeId;
     });
+
+    if (!place) {
+        const error = new Error('Could not find a place for the provided id');
+        error.code = 404;
+        throw error;
+    }
+
     res.json({ place });
 });
 
@@ -30,6 +38,12 @@ router.get('/user/:uid', (req, res, next) => {
     const place = DUMMY_PLACES.find(p => {
         return p.creater === userId;
     });
+
+    if (!place) {
+        const error = new Error('Could not find a place for the provided user id');
+        error.code = 404;
+        return next(error);
+    }
 
     res.json({ place });
 });
