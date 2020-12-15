e601fdee64e4b7fc5bc4f38afac90b71bf4fa91f
sharjeelyunus <sharjeel924@gmail.com>
2020-12-15 16:43:04 +0500

    👌 IMPROVE: Validating API Input (Request Bodies)

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,4 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
+const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
 
@@ -50,6 +51,12 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError('Invalid inputs passed, please check your data.', 422)
+  }
+
   const { title, description, coordinates, address, creator } = req.body;
   // const title = req.body.title;
   const createdPlace = {
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const placesControllers = require('../controllers/places-controller');
 
@@ -8,7 +9,18 @@ router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
-router.post('/', placesControllers.createPlace);
+router.post('/',
+    [
+        check('title')
+            .not()
+            .isEmpty(),
+        check('description').isLength({ min: 5 }),
+        check('address')
+            .not()
+            .isEmpty()
+    ],
+    placesControllers.createPlace
+);
 
 router.patch('/:pid', placesControllers.updatePlace);
 
