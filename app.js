08fc557f6efd5fdeec05b5f162d4b63463feed6f
sharjeelyunus <sharjeel924@gmail.com>
2021-02-28 18:26:50 +0500

    📦 NEW: Connecting Backend to MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -22,7 +23,14 @@ app.use((error, req, res, next) => {
         return next(error);
     }
     res.status(error.code || 500);
-    res.json({message: error.message || 'An unknown error occured!'});
+    res.json({ message: error.message || 'An unknown error occured!' });
 });
 
-app.listen(5000);
+mongoose
+    .connect('mongodb+srv://manu:sharjeel@cluster0.m8g71.mongodb.net/places?retryWrites=true&w=majority')
+    .then(() => {
+        app.listen(5000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
