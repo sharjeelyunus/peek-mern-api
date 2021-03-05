df521b77da8bcee1cf1a0082506b3407c3885952
sharjeelyunus <sharjeel924@gmail.com>
2021-03-05 15:08:58 +0500

    👌 IMPROVE: Updating Places

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -111,7 +111,7 @@ const createPlace = async (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new HttpError('Invalid inputs passed, please check your data.', 422)
@@ -120,14 +120,29 @@ const updatePlace = (req, res, next) => {
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
-  const updatePlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
-  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
-  updatePlace.title = title;
-  updatePlace.description = description;
+  let place;
+  try {
+    place = await Place.findById(placeId)
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not update place.', 500
+    );
+    return next(error);
+  }
 
-  DUMMY_PLACES[placeIndex] = updatePlace;
+  place.title = title;
+  place.description = description;
 
-  res.status(200).json({ place: updatePlace });
+  try {
+    await place.save();
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not update place.', 500
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
 const deletePlace = (req, res, next) => {
