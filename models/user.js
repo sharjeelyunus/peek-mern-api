6033273f31bac2af62478e89afa1b6c7ff4f7a4d
sharjeelyunus <sharjeel924@gmail.com>
2021-03-07 01:53:28 +0500

    👌 IMPROVE: Creating the User Model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 6 },
+    image: { type: String, required: true },
+    places: { type: String, required: true },
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
