c276350749924a88301994aac3fbd7ca4deb464a
sharjeelyunus <sharjeel924@gmail.com>
2021-03-10 22:38:18 +0500

    👌 IMPROVE: Getting Users

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -13,8 +13,18 @@ const DUMMY_USERS = [
   }
 ];
 
-const getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS });
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, '-password')
+  } catch (err) {
+    const error = new HttpError(
+      'Fetching users failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+  res.json({ users: users.map(user => user.toObject({ getters: true })) });
 };
 
 const signup = async (req, res, next) => {
