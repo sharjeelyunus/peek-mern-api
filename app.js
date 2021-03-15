580ef9e983aa05aa4a66026cc8d36d59c225d88a
sharjeelyunus <sharjeel924@gmail.com>
2021-03-16 01:07:07 +0500

    Initial Setup - Connecting to frontend

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-    .connect('mongodb+srv://manu:sharjeel@cluster0.m8g71.mongodb.net/places?retryWrites=true&w=majority')
+    .connect('mongodb+srv://manu:sharjeel@cluster0.m8g71.mongodb.net/mern?retryWrites=true&w=majority')
     .then(() => {
         app.listen(5000);
     })
