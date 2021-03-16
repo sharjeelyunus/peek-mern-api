d90ce6c486c979e8b6eb135e3fab4ad31afe2e23
sharjeelyunus <sharjeel924@gmail.com>
2021-03-17 01:19:46 +0500

    🐛 FIX: Handling CORS Errors

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Controll-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+})
+
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
