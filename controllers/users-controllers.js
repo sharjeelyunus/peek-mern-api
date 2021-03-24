1d2a76051d5ed2e793ba483df285b430b7c62df3
sharjeelyunus <sharjeel924@gmail.com>
2021-03-25 00:59:27 +0500

    👌 IMPROVE: Adding Places (POST)

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -88,7 +88,10 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  res.json({ message: 'Logged in!' });
+  res.json({
+    message: 'Logged in!',
+    user: existingUser.toObject({ getters: true })
+  });
 };
 
 exports.getUsers = getUsers;
