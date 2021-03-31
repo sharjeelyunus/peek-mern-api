3851aa30dbb6f342a6ace49bafcb5c1afd86c941
sharjeelyunus <sharjeel924@gmail.com>
2021-04-01 01:27:53 +0500

    📦 NEW: Using Multer to Save Files

---
+++ middleware/file-upload.js
@@ -0,0 +1,24 @@
+const multer = require('multer');
+const uuid = require('uuid/v1');
+
+const MIME_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpeg',
+};
+
+const fileUpload = multer({
+    limits: 500000,
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, 'uploads/images');
+        },
+        filename: (req, file, cb) => {
+            const ext = MIME_TYPE_MAP[file.mimetype];
+            cb(null, uuid() + '.' + ext);
+        }
+    }),
+    fileFilter: 
+});
+
+module.exports = fileUpload;
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const usersController = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
@@ -9,6 +10,7 @@ router.get('/', usersController.getUsers);
 
 router.post(
     '/signup',
+    fileUpload.single('image'),
     [
         check('name')
             .not()
