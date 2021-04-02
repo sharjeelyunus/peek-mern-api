f2fe49ccd20cf243299a3a1b9dc446213fb17762
sharjeelyunus <sharjeel924@gmail.com>
2021-04-03 00:01:42 +0500

    👌 IMPROVE: Serving Images Statically

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
+const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-var cors = require('cors');
+
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error');
@@ -12,15 +13,18 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(cors(), (req, res, next) => {
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
-    res.setHeader('Access-Controll-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
     next();
-})
+});
 
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
@@ -32,7 +36,7 @@ app.use((req, res, next) => {
 
 app.use((error, req, res, next) => {
     if (req.file) {
-        fs.unlink(req.file.path, (err) => {
+        fs.unlink(req.file.path, err => {
             console.log(err);
         });
     }
@@ -40,11 +44,13 @@ app.use((error, req, res, next) => {
         return next(error);
     }
     res.status(error.code || 500);
-    res.json({ message: error.message || 'An unknown error occured!' });
+    res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 mongoose
-    .connect('mongodb+srv://manu:sharjeel@cluster0.m8g71.mongodb.net/mern?retryWrites=true&w=majority')
+    .connect(
+        `mongodb+srv://manu:sharjeel@cluster0.m8g71.mongodb.net/mern?retryWrites=true&w=majority`
+    )
     .then(() => {
         app.listen(5000);
     })
