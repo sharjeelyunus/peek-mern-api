bdd250349fd9048433deb24da2755bce97244e51
sharjeelyunus <sharjeel924@gmail.com>
2021-04-07 00:53:28 +0500

    👌 IMPROVE: Logging Users (with Hashed Passwords)

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -93,7 +93,26 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
+    const error = new HttpError(
+      'Invalid credentials, could not log you in.',
+      401
+    );
+    return next(error);
+  }
+
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      'Could not log you in, please check your credentials and try again',
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
     const error = new HttpError(
       'Invalid credentials, could not log you in.',
       401
