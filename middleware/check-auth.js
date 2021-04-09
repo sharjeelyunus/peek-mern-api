2b1657291f655af7c342128c707518b89e3e7e9c
sharjeelyunus <sharjeel924@gmail.com>
2021-04-10 01:18:26 +0500

    👌 IMPROVE: Using & Attaching JWT (Tokens)

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,8 +1,11 @@
 const jwt = require('jsonwebtoken');
 
-const HttpError = require("../models/http-error");
+const HttpError = require('../models/http-error');
 
 module.exports = (req, res, next) => {
+    if (req.method === 'OPTIONS') {
+        return next();
+    }
     try {
         const token = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
         if (!token) {
@@ -15,5 +18,4 @@ module.exports = (req, res, next) => {
         const error = new HttpError('Authentication failed!', 401);
         return next(error);
     }
-
 };
