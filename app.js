c34514648435b4a91deece8b116fc36d08c521d1
sharjeelyunus <sharjeel924@gmail.com>
2021-04-23 03:24:38 +0500

    added cors

--- app.js
+++ app.js
@@ -11,11 +11,13 @@ const HttpError = require('./models/http-error');
 
 const app = express();
 
+var cors = require('cors');
+
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
-app.use((req, res, next) => {
+app.use(cors(), (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Headers',
